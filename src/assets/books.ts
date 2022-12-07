2043247f1e505efdf3fca796a87bc54e9a31ce6b
ldrljaca <ermin.h@protonmail.com>
2022-12-07 07:52:18 +0100

    library page design

---
+++ src/assets/books.ts
@@ -0,0 +1,44 @@
+import uuid from "react-uuid";
+
+export const books = [
+    {
+        bookId: uuid(),
+        bookName: "Book Name",
+        bookAuthor: "Book Author",
+        genre: "genre",
+        tags: [
+            "testTag1",
+            "testTag2"
+        ]
+    },
+    {
+        bookId: uuid(),
+        bookName: "Book Name",
+        bookAuthor: "Book Author",
+        genre: "genre",
+        tags: [
+            "testTag1",
+            "testTag2"
+        ]
+    },
+    {
+        bookId: uuid(),
+        bookName: "Book Name",
+        bookAuthor: "Book Author",
+        genre: "genre",
+        tags: [
+            "testTag1",
+            "testTag2"
+        ]
+    },
+    {
+        bookId: uuid(),
+        bookName: "Book Name",
+        bookAuthor: "Book Author",
+        genre: "genre",
+        tags: [
+            "testTag1",
+            "testTag2"
+        ]
+    }
+]
--- src/pages/LibraryPage.tsx
+++ src/pages/LibraryPage.tsx
@@ -1,7 +1,69 @@
-import React from "react";
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  Button,
+  Typography
+} from "@mui/material";
+import { books } from "../assets/books";
 
 const LibraryPage = () => {
-  return <div>Library</div>;
+  return (
+    <Box
+      sx={{
+        position: "relative",
+        width: "89vw",
+        maxHeight: "95vh",
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr 1fr",
+        columnGap: "3%",
+        rowGap: "3%",
+        margin: "10% 0"
+      }}
+    >
+      {books.map(book => (
+        <Card key={book.bookId} sx={{
+          position: "relative",
+          minHeight: "50px",
+          maxHeight: "175px"
+        }}>
+          <CardContent sx={{
+            width: "85%",
+            height: "100%",
+            display: "flex",
+            justifyContent: "space-between",
+            alignContent: "center",
+            border: "5px solid green"
+          }}>
+            <Typography
+              color="text.secondary"
+              gutterBottom
+            >
+              {book.bookName}
+            </Typography>
+
+            <Typography sx={{}}>
+              {book.bookAuthor}
+            </Typography>
+
+            <Typography variant="body2" sx={{
+              border: "5px solid red",
+              display: "flex"
+            }}>{book.tags}</Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small" sx={{
+              position: "absolute",
+              bottom: "0.1%",
+              right: "0.1%"
+            }}>More...</Button>
+          </CardActions>
+        </Card>
+      ))}
+    </Box>
+  );
 };
 
 export default LibraryPage;
+
